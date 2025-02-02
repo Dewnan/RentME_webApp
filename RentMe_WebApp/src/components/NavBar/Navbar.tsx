597269ae79f0226b1e03ace628e46f49b3794320
Dewnan <70105964+Dewnan@users.noreply.github.com>
2025-02-02 08:20:12 +0000

    added simple nav bar and fix the rest later

---
+++ RentMe_WebApp/src/components/NavBar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </ul>
+    </nav>
+    </>
+  );
+}
